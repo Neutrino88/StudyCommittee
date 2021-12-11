513ef7f52a95e401507431614c926cdb8e756b22
Oleg Romanov <romanov-97@mail.ru>
2021-12-11 10:25:34 +0300

    Add verbose names to tables and columns

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -2,20 +2,24 @@ from django.db import models
 
 
 class University(models.Model):
-    name = models.CharField(max_length=200)
-    full_name = models.CharField(max_length=200)
+    name = models.CharField('Краткое название', max_length=200)
+    full_name = models.CharField('Полное название', max_length=200)
     creation_year = models.DateTimeField('Год образования')
 
     class Meta:
         db_table = 'university'
+        verbose_name = 'Университет'
+        verbose_name_plural = 'Университеты'
 
 
 class Faculty(models.Model):
-    name = models.CharField(max_length=200)
-    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=200)
+    university = models.ForeignKey(University, verbose_name='Университет', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'faculty'
+        verbose_name = 'Факультет'
+        verbose_name_plural = 'Факультеты'
 
 
 class Speciality(models.Model):
@@ -24,53 +28,63 @@ class Speciality(models.Model):
     PART_TIME = 'PT'
 
     STUDY_FORMAT_CHOICES = (
-        (FULL_TIME, 'Очное'),
-        (DISTANCE, 'Дистанционное'),
-        (PART_TIME, 'Очно-заочное'),
+        (FULL_TIME, 'Очная'),
+        (DISTANCE, 'Дистанционная'),
+        (PART_TIME, 'Очно-заочная'),
     )
 
-    name = models.CharField(max_length=200)
-    study_format = models.CharField(max_length=2, choices=STUDY_FORMAT_CHOICES, default=FULL_TIME)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=200)
+    study_format = models.CharField('Формат обучения', max_length=2, choices=STUDY_FORMAT_CHOICES, default=FULL_TIME)
+    faculty = models.ForeignKey(Faculty, verbose_name='Факультет', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'speciality'
+        verbose_name = 'Специальность'
+        verbose_name_plural = 'Специальности'
 
 
 class StudyGroup(models.Model):
-    number = models.CharField(max_length=10)
-    course_number = models.PositiveIntegerField()
-    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    number = models.CharField('Номер группы', max_length=10)
+    course_number = models.PositiveSmallIntegerField('Номер курса')
+    speciality = models.ForeignKey(Speciality, verbose_name='Специальность', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'study_group'
+        verbose_name = 'Учебная группа'
+        verbose_name_plural = 'Учебные группы'
 
 
 class PersonalInfo(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    patronymic_name = models.CharField(max_length=30)
-    birthday = models.DateField()
+    first_name = models.CharField('Имя', max_length=30)
+    last_name = models.CharField('Фамилия', max_length=30)
+    patronymic_name = models.CharField('Отчество', max_length=30)
+    birthday = models.DateField('Дата рождения')
 
     class Meta:
         db_table = 'personal_info'
+        verbose_name = 'Личная информация'
+        verbose_name_plural = 'Личная информация'
 
 
 class Student(models.Model):
-    info = models.OneToOneField(PersonalInfo, on_delete=models.CASCADE)
-    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE)
+    info = models.OneToOneField(PersonalInfo, verbose_name='Личная информация', on_delete=models.CASCADE)
+    group = models.ForeignKey(StudyGroup, verbose_name='Учебная группа', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'student'
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
 
 
 class Lecturer(models.Model):
-    info = models.OneToOneField(PersonalInfo, on_delete=models.CASCADE)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
-    degree = models.CharField(max_length=30)
+    info = models.OneToOneField(PersonalInfo, verbose_name='Личная информация', on_delete=models.CASCADE)
+    faculty = models.ForeignKey(Faculty, verbose_name='Факультет', on_delete=models.CASCADE)
+    degree = models.CharField('Степень', max_length=30)
 
     class Meta:
         db_table = 'lecturer'
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
 
 
 class Discipline(models.Model):
@@ -84,29 +98,35 @@ class Discipline(models.Model):
         (TEST, 'Тест'),
     )
 
-    name = models.CharField(max_length=200)
-    control_form = models.CharField(max_length=2, choices=CONTROL_FORM_CHOICES, default=CREDIT)
-    lecture_hours = models.PositiveIntegerField()
-    practice_hours = models.PositiveIntegerField()
+    name = models.CharField('Название дисциплины', max_length=200)
+    control_form = models.CharField('Форма контроля', max_length=2, choices=CONTROL_FORM_CHOICES, default=CREDIT)
+    lecture_hours = models.PositiveIntegerField('Теоритические часы')
+    practice_hours = models.PositiveIntegerField('Практические часы')
 
     class Meta:
         db_table = 'discipline'
+        verbose_name = 'Учебная дисциплина'
+        verbose_name_plural = 'Учебные дисциплины'
 
 
 class LecturerGroupDiscipline(models.Model):
-    professor = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
-    group = models.ForeignKey(StudyGroup, on_delete=models.CASCADE)
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Lecturer, verbose_name='Преподаватель', on_delete=models.CASCADE)
+    group = models.ForeignKey(StudyGroup, verbose_name='Учебная группа', on_delete=models.CASCADE)
+    discipline = models.ForeignKey(Discipline, verbose_name='Дисциплина', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'lecturer_group_discipline'
         unique_together = ('professor', 'group', 'discipline')
+        verbose_name = 'Преподаватель-группа-дисциплина'
+        verbose_name_plural = 'Преподаватель-группа-дисциплина'
 
 
 class SpecialityDiscipline(models.Model):
-    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
-    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+    speciality = models.ForeignKey(Speciality, verbose_name='Специальность', on_delete=models.CASCADE)
+    discipline = models.ForeignKey(Discipline, verbose_name='Дисциплина', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'speciality_discipline'
         unique_together = ('speciality', 'discipline')
+        verbose_name = 'Специальность-дисциплина'
+        verbose_name_plural = 'Специальность-дисциплина'
