685eec18a17cdf255f62f4b6e4066eafbd745670
Oleg Romanov <romanov-97@mail.ru>
2021-12-11 12:45:30 +0300

    Add base endpoints and templates for index, university, faculty, speciality anf group pages

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /
     path('', views.index, name='index'),
+    # ex: /university/1/
+    path('university/<int:univ_id>/', views.university_page, name='university'),
+    # ex: /faculty/2/
+    path('faculty/<int:fac_id>/', views.faculty_page, name='faculty'),
+    # ex: /speciality/3/
+    path('speciality/<int:spec_id>/', views.speciality_page, name='speciality'),
+    # ex: /group/4/
+    path('group/<int:group_id>/', views.group_page, name='group'),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,5 +1,29 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import University
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    universities = University.objects.order_by('short_name')[:20]
+    context = {'universities': universities}
+    return render(request, 'app/index.html', context)
+
+
+def university_page(request, univ_id):
+    return HttpResponse(f"University with id {univ_id}")
+
+
+def faculty_page(request, fac_id):
+    response = f"faculty id = {fac_id}"
+    return HttpResponse(response)
+
+
+def speciality_page(request, spec_id):
+    response = f"spec_id = {spec_id}"
+    return HttpResponse(response)
+
+
+def group_page(request, group_id):
+    response = f"group_id = {group_id}"
+    return HttpResponse(response)
