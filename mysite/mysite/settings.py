b47c8ce48cff3c53c19e3e01ec871af607de8ae1
Oleg Romanov <romanov-97@mail.ru>
2021-12-15 21:09:46 +0300

    Add login and logout pages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -113,6 +113,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
